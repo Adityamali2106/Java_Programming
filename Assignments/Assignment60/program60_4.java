0f173d3ca029f5042660a1138157ca833368f89e
Aditya Mali <134832499+Adityamali2106@users.noreply.github.com>
2026-01-30 13:18:39 +0530

    Add files via upload

---
+++ Assignments/Assignment60/program60_4.java
@@ -0,0 +1,53 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//    Program Name : program60_2
+//    Description  : Accept the Directory Name and get the extension then
+//                   display only files having that extension.
+//    Author Name  : Aditya Vijay Mali
+//    Date         : 30/01/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+import java.io.*;
+
+class program60_4
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the Directory name : ");
+        String DirName = sobj.nextLine();
+
+        System.out.println("Enter the file extension : ");
+        String Extension = sobj.nextLine();
+
+        try
+        {
+            File fobj = new File(DirName);
+
+            if ((fobj.exists()) && (fobj.isDirectory()))
+            {
+                File fArr[] = fobj.listFiles();
+
+                for(int i = 0; i < fArr.length;i++)
+                {
+                    if(fArr[i].isFile()&& fArr[i].getName().endsWith(Extension))
+                    {
+                        System.out.println("File Name is : "+fArr[i].getName());
+                    }
+                }
+            }
+            else
+            {
+                System.out.println("There is no such Folder ");
+            }
+        }
+        catch(Exception E)
+        {
+            System.out.println(E.getMessage());
+        }
+
+    }
+}
