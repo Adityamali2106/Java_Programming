580042771acbed393ef8b3429f4d4ad409fc6933
Aditya Mali <134832499+Adityamali2106@users.noreply.github.com>
2026-02-23 06:27:04 +0530

    Add files via upload

---
+++ Assignments/Assignment_73/program73_3.java
@@ -0,0 +1,135 @@
+import java.util.*;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name 	    : Matrix
+//  Discription 	: It is used to perform multiple operations on 2d Array
+//  Author 			: Aditya Vijay Mali
+//  Date 			: 23/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Matrix
+{
+    public int iRow;
+    public int iCol;
+    public int Arr[][];
+
+    public Matrix(int a,int b)
+    {
+        this.iRow = a;
+        this.iCol = b;
+
+        Arr = new int[iRow][iCol];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  Function Name 	: Accept
+    //  Discription 	: It is used to accept the values from user and store it into matrix
+    //  Input 			: Nothing
+    //  Output 			: Nothing
+    //  Author 			: Aditya Vijay Mali
+    //  Date 			: 23/02/2026
+    //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    public void Accept()
+    {
+        int i = 0,j = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the elements of Matrix : ");
+
+        for (i = 0; i < iRow; i++) 
+        {
+            System.out.println("Emter the elements of row : "+(i+1));
+            for(j = 0;j < iCol;j++)
+            {
+                Arr[i][j] = sobj.nextInt();                
+            }    
+        }
+
+        sobj.close();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  Function Name 	: Display
+    //  Discription 	: It is used to Dispaly the values of matrix on screen
+    //  Input 			: Nothing
+    //  Output 			: Nothing
+    //  Author 			: Aditya Vijay Mali
+    //  Date 			: 23/02/2026
+    //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void Display()
+    {
+        int i = 0,j = 0;
+
+        System.out.println("elements of Matrix Are : ");
+
+        for (i = 0; i < iRow; i++) 
+        {
+            for(j = 0;j < iCol;j++)
+            {
+                System.out.print(Arr[i][j]+"\t");      
+            }
+            System.out.println(); 
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  Function Name 	: ReverseColumns
+    //  Discription 	: It is used to reverse the each Columns of a marix
+    //  Input 			: Nothing
+    //  Output 			: Nothing
+    //  Author 			: Aditya Vijay Mali
+    //  Date 			: 23/02/2026
+    //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void ReverseColumns()
+    {
+        int i = 0,j = 0;
+        int iTemp = 0;
+
+        for (i = 0; i < iCol; i++) 
+        {
+            for (j = 0; j < iRow / 2; j++) 
+            {
+                iTemp = Arr[i][j];
+                Arr[i][j] = Arr[iRow -1 - i][j];
+                Arr[iRow - 1 - i][j] = iTemp;
+            }
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name 	    : program73_3
+//  Discription 	: It is the entry point class for the application
+//  Author 			: Aditya Vijay Mali
+//  Date 			: 23/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class program73_3
+{
+    public static void main(String A[]) 
+    {
+        Matrix mobj = new Matrix(4, 4);
+
+        mobj.Accept();
+        mobj.Display();
+
+        mobj.ReverseRows();
+        System.out.println("After Reversing each Row of Matrix is : ");
+        mobj.Display();
+    }
+}
---
+++ Assignments/Assignment_73/program73_4.java
@@ -0,0 +1,147 @@
+import java.util.*;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name 	    : Matrix
+//  Discription 	: It is used to perform multiple operations on 2d Array
+//  Author 			: Aditya Vijay Mali
+//  Date 			: 23/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Matrix
+{
+    public int iRow;
+    public int iCol;
+    public int Arr[][];
+
+    public Matrix(int a,int b)
+    {
+        this.iRow = a;
+        this.iCol = b;
+
+        Arr = new int[iRow][iCol];
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  Function Name 	: Accept
+    //  Discription 	: It is used to accept the values from user and store it into matrix
+    //  Input 			: Nothing
+    //  Output 			: Nothing
+    //  Author 			: Aditya Vijay Mali
+    //  Date 			: 23/02/2026
+    //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    public void Accept()
+    {
+        int i = 0,j = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the elements of Matrix : ");
+
+        for (i = 0; i < iRow; i++) 
+        {
+            System.out.println("Emter the elements of row : "+(i+1));
+            for(j = 0;j < iCol;j++)
+            {
+                Arr[i][j] = sobj.nextInt();                
+            }    
+        }
+
+        sobj.close();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  Function Name 	: Display
+    //  Discription 	: It is used to Dispaly the values of matrix on screen
+    //  Input 			: Nothing
+    //  Output 			: Nothing
+    //  Author 			: Aditya Vijay Mali
+    //  Date 			: 23/02/2026
+    //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void Display()
+    {
+        int i = 0,j = 0;
+
+        System.out.println("elements of Matrix Are : ");
+
+        for (i = 0; i < iRow; i++) 
+        {
+            for(j = 0;j < iCol;j++)
+            {
+                System.out.print(Arr[i][j]+"\t");      
+            }
+            System.out.println(); 
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //  Function Name 	: ReverseColumns
+    //  Discription 	: It is used to check the matrix is identity matrix or not
+    //  Input 			: Nothing
+    //  Output 			: Identity matrix or not Identity matrix (true/false)
+    //  Author 			: Aditya Vijay Mali
+    //  Date 			: 23/02/2026
+    //
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    public boolean isIdentity() 
+    {
+        boolean bFlag = true;
+        int i = 0, j = 0;
+
+        for (i = 0; i < iRow; i++) 
+        {
+            for (j = 0; j < iCol; j++) 
+            {
+                if (i == j && Arr[i][j] != 1) 
+                {
+                    bFlag = false;
+                }
+                if (i != j && Arr[i][j] != 0) 
+                {
+                    bFlag = false;
+                }
+            }
+        }
+
+        return bFlag;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name 	    : program73_4
+//  Discription 	: It is the entry point class for the application
+//  Author 			: Aditya Vijay Mali
+//  Date 			: 23/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class program73_4
+{
+    public static void main(String A[]) 
+    {
+        Matrix mobj = new Matrix(4, 4);
+
+        mobj.Accept();
+        mobj.Display();
+
+        if (mobj.isIdentity())
+        {
+            System.out.println("Matrix is identity matrix");
+        }
+        else
+        {
+            System.out.println("Matrix is NOt identity matrix");
+        }
+    }
+}
