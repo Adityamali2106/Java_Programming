57dabab4019fce6a3ac22d2c5621f807a3b4f0b5
Aditya Mali <134832499+Adityamali2106@users.noreply.github.com>
2026-05-19 18:48:11 +0530

    Add files via upload

---
+++ ClassWork/Day_32/program603.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+class program604
+{
+    public static void main(String A[]) throws Exception
+    {
+        String Header = null;
+        int iRet = 0;
+        int i = 0, j = 0;
+        byte Buffer[] = new byte[1024];
+        byte Key = 0x11;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the name of Folder :");
+        String FolderName = sobj.nextLine();
+
+        System.out.println("Enter the name of Packed File :");
+        String PackName = sobj.nextLine();
+
+        File fobj = new File(FolderName);
+
+        if((fobj.exists()) && (fobj.isDirectory()))
+        {
+            File packObj = new File(PackName);
+
+            packObj.createNewFile();
+
+            FileOutputStream foobj = new FileOutputStream(packObj);
+            
+            FileInputStream fiobj = null;
+
+            System.out.println("Folder is persent ");
+
+            File fArr[] = fobj.listFiles();
+
+            System.out.println("Number of Files in the folder are : "+fArr.length);
+
+            for(i = 0; i < fArr.length;i++)
+            {
+                fiobj = new FileInputStream(fArr[i]);
+
+                if(fArr[i].getName().endsWith(".txt") == true)
+                {
+                    // Header formation
+                    Header = fArr[i].getName() + " " + fArr[i].length();
+
+                    for(j = Header.length();j < 100;j++)
+                    {
+                        Header = Header + " ";
+                    }
+
+                    System.out.println("Header : "+Header+"Length : "+Header.length());
+                
+                    /*
+                    while((iRet = fiobj.read(Buffer)) != -1)
+                    {
+                        //Encryption Logic
+                        for (j = 0; j < iRet; j++)
+                        {
+                            Buffer[j] = (byte)(Buffer[j] ^ Key);
+                        }
+
+                        foobj.write(Buffer,0,iRet);
+                    }
+                    */
+                }
+
+                fiobj.close();
+            }
+            foobj.close();
+        }
+        else
+        {
+            System.out.println("There is no such Folder ");
+        }
+    }
+}
